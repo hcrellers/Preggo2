b4a6dec30644c73bab5c4613225ff18f91e94954
Halie <halie.creollo@gmail.com>
2019-08-19 03:04:55 -0500

    api nutrient search working

--- mern-preggo-app/src/components/Food Log/create-foodlog.component.js
+++ mern-preggo-app/src/components/Food Log/create-foodlog.component.js
@@ -6,23 +6,34 @@ const CreateFoodLog = () => {
     const APP_KEY = "e806ed0b36e74af63bf3578a2565f6ef";
 
     const [nutrition, setNutrition] = useState([]);
-  
+    const [search, setSearch] = useState("");
+    const [query, setQuery] = useState('chicken')
 
     useEffect( () => {
         getNutrition();
-    }, []);
+    }, [query]);
 
     const getNutrition = async () => {
-        const response = await fetch(`https://api.edamam.com/api/food-database/parser?nutrition-type=logging&ingr=red%20apple&app_id=${APP_ID}&app_key=${APP_KEY}`)
+        const response = await fetch(`https://api.edamam.com/api/food-database/parser?nutrition-type=logging&ingr=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
         const data = await response.json();
         console.log(data.hints[0].food.nutrients);
         setNutrition(data.hints[0].food.nutrients);
     };
 
+    const updateSearch = e => {
+        setSearch(e.target.value);
+        // console.log(search);
+    }
+    const getSearch = e => {
+        e.preventDefault();
+        setQuery(search);
+        setSearch("")
+    }
+
     return (
         <div className="CreateFoodLog">
-            <form className="search-form">
-                <input className="search-bar" type="text" />
+            <form onSubmit={getSearch} className="search-form">
+                <input className="search-bar" type="text" value={search} onChange={updateSearch} />
                 <button className="search-button" type="submit">
                     Search
                  </button>
@@ -30,7 +41,7 @@ const CreateFoodLog = () => {
             </form>
       {/* {nutrition.map(nutrients =>(  */}
                 <Nutrition />
-            {/* // ))}  */}
+             {/* ))}   */}
         </div>
     );
 };
